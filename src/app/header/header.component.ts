4f3e1e8bb9eace887a5377743066145c256642fb
Diego Torres Mijarra <134062570+DiegoTorresMijarra@users.noreply.github.com>
2024-04-09 11:44:11 +0200

    header:html y ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IonicModule, IonRadio, IonToggle} from "@ionic/angular";
+import {FormBuilder, FormsModule} from "@angular/forms";
+import {subscribeOn} from "rxjs";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    IonicModule,
+    FormsModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  radioDarkMode= false;
+  ngOnInit(): void {
+    this.radioDarkMode =  window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.setDefaultMode()
+  }
+
+  protected setDefaultMode() {
+    if(this.radioDarkMode) document.body.classList.add('dark');
+  }
+
+  protected toggleDarkMode(): void {
+    console.log('toggleDarkMode')
+    document.body.classList.toggle('dark');
+  }
 
 }
