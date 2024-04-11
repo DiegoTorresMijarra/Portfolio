a5d1a1130caadd9f986567147b3d96fd5452100f
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-04-11 15:04:36 +0200

    init header-body component

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ContentHeaderComponent} from "../content-header/content-header.component";
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [
+    ContentHeaderComponent
+  ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
