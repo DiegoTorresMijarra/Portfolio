eb32eec75a3db3b337be1b3d23d390444533d3e8
Diego Torres Mijarra <diego.torres@alumno.iesluisvives.org>
2024-04-17 07:50:36 +0200

    min footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    IonicModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
